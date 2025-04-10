109b47f08e681196a888e6e88583801acc30d8b5
Beatriz <mbmbv@ic.ufal.br>
2025-04-09 22:31:25 -0300

    task 07

--- tasks/task-07-histogram-matching/task-07-histogram-matching.py
+++ tasks/task-07-histogram-matching/task-07-histogram-matching.py
@@ -30,4 +30,45 @@ import scikitimage as ski
 
 def match_histograms_rgb(source_img: np.ndarray, reference_img: np.ndarray) -> np.ndarray:
     # Your implementation here
-    pass
+    def get_gray_histogram(img):
+        if (len(img.shape) == 3):
+            raise Exception("Must be grayscale!")
+  
+        return cv.calcHist([img], [0], None, [256], [0, 256])
+    
+    def calc_cdf(histogram):
+        cdf = histogram.cumsum()
+        cdf_normalized = cdf / cdf[-1]
+        return cdf_normalized
+
+    def match_gray_histograms(source, reference):
+        source_hist = get_gray_histogram(source)
+        reference_hist = get_gray_histogram(reference)
+
+        source_cdf = calc_cdf(source_hist)
+        reference_cdf = calc_cdf(reference_hist)
+
+        look_up_table = np.zeros(256, dtype=np.uint8)
+
+        for value in range(256):
+            diff = np.abs(reference_cdf - source_cdf[value])
+            matched = np.argmin(diff)
+            look_up_table[value] = matched
+
+        matched_channel = cv.LUT(source, look_up_table)
+        return matched_channel
+
+    def match_rgb_histogram(source, reference):
+        matched_channels = []
+
+        for i in range(3):
+            source_channel = source[:, :, i] 
+            reference_channel = reference[:, :, i] 
+
+            matched = match_gray_histograms(source_channel, reference_channel)
+            matched_channels.append(matched)
+
+        return cv.merge(matched_channels)
+
+    output = match_rgb_histogram(source_img, reference_img)
+    return output
